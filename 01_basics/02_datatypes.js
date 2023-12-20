06cf20f60a0a139b0cf8cc67265aa0f7448e6874
Rathore85 <136048075+Rathore85@users.noreply.github.com>
2023-12-20 05:47:19 -0500

    Add files via upload

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // to treat all JS code as newer version
+
+// alert(3+3)// we are using nodejs, not browser
+
+console.log(3+9);
+
+console.log("Prashant")
+
+let name = "Prashant"
+let age = 23
+let isLoggedIn = false
+let state; 
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false 
+//  null => standalone value
+//  undefined =>
+// symbol => unique
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
