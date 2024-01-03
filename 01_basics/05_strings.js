5015f768152794f1388aa46a28e8717895753d95
Rathore85 <136048075+Rathore85@users.noreply.github.com>
2024-01-03 04:24:18 -0500

    Add files via upload

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,40 @@
+const name = "Ayush"
+const currentAge = 7
+
+// console.log(name + currentAge + " Value")
+
+// console.log(`Hello my name is ${name} and my current age is ${currentAge}`);
+
+const nickName = new String('Mave-rick') 
+
+// console.log(nickName [0]);
+// console.log(nickName.length);
+// console.log(nickName.toUpperCase());
+// console.log(nickName.bold());
+// console.log(nickName.anchor());
+// console.log(nickName.big());
+// console.log(nickName.concat());
+// console.log(nickName.endswith());
+// console.log(nickName.italics());
+// console.log(nickName.toLowerCase());
+
+// console.log(nickName.charAt(2));
+// console.log(nickName.indexOf('v'));
+
+// const newString = nickName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = nickName.slice(-8,4)
+// console.log(anotherString);
+
+// const newStringOne = "  Ayush  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://ayush.com/ayush%20rathore"
+
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('jai'));
+
+console.log(nickName.split('-'));
